b30e15627c1b07fa5604962443f000ae92dd0783
TheRedRover <mykhno2001@gmail.com>
2021-08-20 04:21:02 +0300

    update: matrix output and operator += are added

--- test/test.cc
+++ test/test.cc
@@ -4,10 +4,18 @@
 
 int main()
 {
-    Matrix<int> matrix = Matrix<int>({1,2,3,4,5,6},2,3);
+    Matrix<double> m2 = Matrix<double>({1,2,3,4,5,6},2,3);
+
+    Matrix<double> matrix = Matrix<double>({1,2,3,4,5,6},2,3);
+
 
     std::cout<<(matrix(0,2)==3)<<"\n";
     matrix(0,2) = 6;
     std::cout<<(matrix(0,2)==6)<<"\n";
+
+    matrix+=m2;
+    std::cout<<(matrix(1,2)==12)<<"\n";
+
+    std::cout<<matrix;    
 }
 
