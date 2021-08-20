23320f9ffecd4f1c02195582dbc5d2b897d8a9e1
Clang Format <clang-format@1337z.ninja>
2021-08-20 09:56:12 +0000

    apply clang-format

--- include/pdmatrix.hh
+++ include/pdmatrix.hh
@@ -3,62 +3,55 @@
 #ifndef LIBPDMATRIX_PDMATRIX_
 #define LIBPDMATRIX_PDMATRIX_
 
-#include <vector>
-#include <type_traits>
 #include <cstddef>
 #include <initializer_list>
+#include <iomanip>
 #include <iostream>
 #include <ostream>
-#include <iomanip>
+#include <type_traits>
+#include <vector>
 
-
-
-template <typename T>
-struct Matrix {
-    static_assert(std::is_arithmetic<T>::value, "Arithmetic type is required");
-    using matrix = std::vector<T>;
+template <typename T> struct Matrix {
+  static_assert(std::is_arithmetic<T>::value, "Arithmetic type is required");
+  using matrix = std::vector<T>;
 
 private:
-    matrix matrix_;
-    size_t cols_;
-    size_t rows_;
+  matrix matrix_;
+  size_t cols_;
+  size_t rows_;
 
 public:
-    Matrix<T>(std::initializer_list<T> l, size_t rows, size_t cols): matrix_(l), rows_(rows), cols_(cols){};
+  Matrix<T>(std::initializer_list<T> l, size_t rows, size_t cols)
+      : matrix_(l), rows_(rows), cols_(cols){};
 
-    auto cols() const -> size_t { return cols_ ;}
+  auto cols() const -> size_t { return cols_; }
 
-    auto rows() const -> size_t { return rows_;}
+  auto rows() const -> size_t { return rows_; }
 
-    auto operator()(const size_t i, size_t j) -> T&
-    {
-        return matrix_[i*cols()+j];
-    };
+  auto operator()(const size_t i, size_t j) -> T & {
+    return matrix_[i * cols() + j];
+  };
 
-    auto operator+=(const Matrix<T>& m) -> Matrix<T> *
-    {
-        if(!(this->rows()==m.rows() && this->cols()==m.cols()))
-            throw "matrices have different size";
-        for(auto i=matrix_.begin(), j=m.matrix_.begin(); i!=matrix_.end();i++,j++)
-        {
-            *i = *i+*j;
-        }
-        return this;
+  auto operator+=(const Matrix<T> &m) -> Matrix<T> * {
+    if (!(this->rows() == m.rows() && this->cols() == m.cols()))
+      throw "matrices have different size";
+    for (auto i = matrix_.begin(), j = m.matrix_.begin(); i != matrix_.end();
+         i++, j++) {
+      *i = *i + *j;
     }
+    return this;
+  }
 };
 
-template<typename T>
-auto operator<<(std::ostream& os, Matrix<T>& m) -> std::ostream&
-{
-    for(auto i = 0; i<m.rows(); i++)
-    {
-        for(auto j=0; j<m.cols(); j++)
-        {
-            os<<std::setprecision(6)<<m(i,j)<<"\t";
-        }
-        os<<"\n\n";
+template <typename T>
+auto operator<<(std::ostream &os, Matrix<T> &m) -> std::ostream & {
+  for (auto i = 0; i < m.rows(); i++) {
+    for (auto j = 0; j < m.cols(); j++) {
+      os << std::setprecision(6) << m(i, j) << "\t";
     }
-    return os;
+    os << "\n\n";
+  }
+  return os;
 }
 
 #endif // LIBPDMATRIX_PDMATRIX_
--- test/test.cc
+++ test/test.cc
@@ -2,20 +2,17 @@
 
 #include <iostream>
 
-int main()
-{
-    Matrix<double> m2 = Matrix<double>({1,2,3,4,5,6},2,3);
+int main() {
+  Matrix<double> m2 = Matrix<double>({1, 2, 3, 4, 5, 6}, 2, 3);
 
-    Matrix<double> matrix = Matrix<double>({1,2,3,4,5,6},2,3);
+  Matrix<double> matrix = Matrix<double>({1, 2, 3, 4, 5, 6}, 2, 3);
 
+  std::cout << (matrix(0, 2) == 3) << "\n";
+  matrix(0, 2) = 6;
+  std::cout << (matrix(0, 2) == 6) << "\n";
 
-    std::cout<<(matrix(0,2)==3)<<"\n";
-    matrix(0,2) = 6;
-    std::cout<<(matrix(0,2)==6)<<"\n";
+  matrix += m2;
+  std::cout << (matrix(1, 2) == 12) << "\n";
 
-    matrix+=m2;
-    std::cout<<(matrix(1,2)==12)<<"\n";
-
-    std::cout<<matrix;    
+  std::cout << matrix;
 }
-
